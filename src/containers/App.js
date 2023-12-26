1c88e40aa34b7c5443589d1d4afe3e36c3d70a8d
brosdee21 <nwaokolodennis90@gmail.com>
2023-12-26 14:38:51 +0100

    first commit

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,9 @@
 import React, {Component} from "react";
-import CardList from "./CardList";
-import SearchBox from './SearchBox';
-import './App.css';
-import Scroll from "./Scroll";
+import CardList from "../components/CardList";
+import SearchBox from '../components/SearchBox';
+import '../containers/App.css';
+import Scroll from "../components/Scroll";
+import ErrorBoundry from "../components/ErrorBoundry";
 
 
 class App extends Component {
@@ -16,7 +17,8 @@ class App extends Component {
     }
 
     componentDidMount(){
-        fetch('https://jsonplaceholder.typicode.com/users').then(Response => Response.json())
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(Response => Response.json())
         .then(users => this.setState({robots: users}));
         
         
@@ -28,27 +30,28 @@ class App extends Component {
     }
 
     render() {
-        const filteredRobots = this.state.robots.filter(robots => {
-            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        const { robots, searchfield } = this.state;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         }) 
 
-        if (this.state.robots.length === 0){
-            return <h1>Loading</h1>
-        }else{
-        
-        return(
+        return !robots.length ?
+            <h1>Loading</h1> :
+(
             <div className="tc">
             <h1 className="f1">RoboFriends</h1>
             <SearchBox searchChange={this.onSearchChange}/>
 
            <Scroll>
+           <ErrorBoundry>
             <CardList robots={filteredRobots} />
+            </ErrorBoundry>
             </Scroll>
             </div>
         );
     }
 }
-}
+
 
 export default App;
 
